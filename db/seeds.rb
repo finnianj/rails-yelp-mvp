203441575498b539896ce5f7cebe9b719ef6195d
Finnian Charlton-Jones <charltonjones.fj@gmail.com>
2022-11-16 15:23:09 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+10.times do
+  Restaurant.create!(name: Faker::Games::ElderScrolls.creature, address:
+    Faker::Games::Fallout.location, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: "01268745983" )
+end
